e0b3247360e5eaf45a2273112cd328e508b989a8
Varun Reddy Mamidala <mvarunreddy1401@gmail.com>
2026-02-17 11:35:36 -0700

    logging and exception files added

---
+++ demo.py
@@ -0,0 +1,30 @@
+
+# Below code is to check the logging config
+# from src.logger import logging
+
+# logging.debug("This is a debug message.")
+# logging.info("This is an info message.")
+# logging.warning("This is a warning message.")
+# logging.error("This is an error message.")
+# logging.critical("This is a critical message.")
+
+# --------------------------------------------------------------------------------
+
+# # Below code is to check the exception config
+# This will raise an error and raise an exception in the log folder
+from src.logger import logging
+from src.exception import MyException
+import sys
+
+try:
+    a = 1+'Z'
+except Exception as e:
+    logging.info(e)
+    raise MyException(e, sys) from e
+
+# --------------------------------------------------------------------------------
+
+# from src.pipline.training_pipeline import TrainPipeline
+
+# pipline = TrainPipeline()
+# pipline.run_pipeline()
