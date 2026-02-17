5019de5190914f9f41dbb37172652d83983078fa
Varun Reddy Mamidala <mvarunreddy1401@gmail.com>
2026-02-16 19:51:55 -0700

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Varun Reddy",
+    author_email="mvarunreddy1401@gmail.com",
+    packages=find_packages()
+)
